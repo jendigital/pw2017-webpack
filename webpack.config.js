618ffe4c397d2e431f8b86ef14a53d5273e19fa1
Christophe Porteneuve <tdd@tddsworld.com>
2017-10-06 16:36:27 +0200

    Étape 4 : Modulariser la configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,46 @@
+const merge = require('webpack-merge')
 const path = require('path')
-const webpack = require('webpack')
+const parts = require('./webpack.parts')
 
 const PATHS = {
   app: path.join(__dirname, 'src'),
   target: path.join(__dirname, 'build'),
 }
 
-module.exports = {
+const CORE_CONFIG = {
   devServer: {
     contentBase: PATHS.target,
     hot: true,
     overlay: true,
   },
+  devtool: 'cheap-module-source-map',
   entry: PATHS.app,
   output: {
     path: PATHS.target,
     filename: 'bundle.js',
+    publicPath: '/',
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1 },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [require('autoprefixer')()],
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1 },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [require('autoprefixer')()],
-              sourceMap: true,
-            },
-          },
-          { loader: 'sass-loader', options: { sourceMap: true } },
-        ],
-      },
-      {
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1 },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [require('autoprefixer')()],
-              sourceMap: true,
-            },
-          },
-          { loader: 'stylus-loader', options: { sourceMap: true } },
-        ],
-      },
-    ],
-  },
-  devtool: 'cheap-module-source-map',
 }
+
+const devConfig = () =>
+  merge([
+    CORE_CONFIG,
+    parts.hmr(),
+    parts.loadCSS(),
+    parts.loadSASS(),
+    parts.loadStylus(),
+  ])
+
+const prodConfig = () =>
+  merge([
+    CORE_CONFIG,
+    parts.extractCSS(),
+    parts.extractSASS(),
+    parts.extractStylus(),
+    {
+      devtool: 'source-map',
+    },
+  ])
+
+module.exports = (env = process.env.NODE_ENV) =>
+  env === 'production' ? prodConfig() : devConfig()
